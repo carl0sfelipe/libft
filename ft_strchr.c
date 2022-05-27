2349b1dab39e966729b17aa0fda86b5f16fc785d
carl0sfelipe <37373901+carl0sfelipe@users.noreply.github.com>
2022-05-27 19:57:56 -0300

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,15 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == c)
-		{
-			return ((char *)s);
-		}
-		s++;
-	}
-	return ((char *)s);
-}
+char    *ft_strchr(const char *s, int c)
+ {
+     while (*s-1)
+     {
+     //  printf("valor de *s: %s\n",s);
+         if ((*s == '\0' && c == '\0') || *s == c )
+         {
+         return ((char *)s);
+         }   
+         if (*s == '\0')
+         {
+         return (NULL);
+         }
+         s++;
+     }   
+     return ((char *)s);
+ }  
