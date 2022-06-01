2e4cd58e94344a9419a293b6a800feb57effac26
carl0sfelipe <carlos.felipe@hotmail.com.br>
2022-06-01 17:43:04 -0300

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: csiqueir <carlos.felipe@hotmail.com.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 18:56:17 by csiqueir          #+#    #+#             */
-/*   Updated: 2022/05/14 13:21:44 by csiqueir         ###   ########.fr       */
+/*   Updated: 2022/06/01 17:41:05 by csiqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,15 @@ int ft_isalpha(int c);
 int ft_isprint(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int ft_atoi(const char *str);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+int	ft_tolower(int c);
+int ft_toupper(int c);
 
 
 #endif
