81686e6988370461ab648b34b50a406b5460bef1
carl0sfelipe <carlos.felipe@hotmail.com.br>
2022-06-09 18:11:42 -0300

    memmove fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: csiqueir <csiqueir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 18:24:42 by csiqueir          #+#    #+#             */
-/*   Updated: 2022/06/08 15:51:57 by csiqueir         ###   ########.fr       */
+/*   Updated: 2022/06/09 18:11:20 by csiqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,25 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*dest;
-	size_t	i;
-	char	*source;
+	unsigned char	*dest;
+	size_t			i;
+	unsigned char	*source;
 
-	source = (char *)src;
-	dest = (char *)dst;
+	source = (unsigned char *)src;
+	dest = (unsigned char *)dst;
 	i = 0;
 	if (dst == NULL && src == NULL)
-		return (0);
+		return (NULL);
 	if (dest > source)
-		while (len-- > 0)
+		while (len--)
 			dest[len] = source[len];
 	else
-		while (i++ < len)
+	{
+		while (i < len)
+		{
 			dest[i] = source[i];
+			i++;
+		}
+	}
 	return (dest);
 }
-/*
-#include <stdio.h>
-#include <string.h>
-
-int main () {
-   char dest[50] = "helloworld";
-   const char src[]  = "warIIIIIIIIIIIIII";
-
-   printf("Before memmove dest = %s, src = %s\n", dest, src);
-   memmove(dest, src, 10);
-   printf("After memmove dest = %s, src = %s\n", dest, src);
-   char dest1[50] = "helloworld";
-   const char src1[]  = "warIIIIIIIIIIIIII";
-
-
-   printf("Before memcpy  dest = %s, src = %s\n", dest1, src1);
-   ft_memmove(dest1, src1, 10);
-   printf("After memcpy  dest = %s, src = %s\n", dest1, src1);
- 
-
-
-   return(0);
-}*/
