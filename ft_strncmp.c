7df005d36d10ba7fba895b0614a0a3af8a0c71f3
carl0sfelipe <carlos.felipe@hotmail.com.br>
2022-06-13 18:09:57 -0300

    strcmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csiqueir <csiqueir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 18:04:56 by csiqueir          #+#    #+#             */
+/*   Updated: 2022/06/13 18:09:09 by csiqueir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int     ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-        size_t  i;
+	size_t	i;
 
-        i = 0;
-        if (n == 0)
-                return (0);
-        while (s1[i] == s2[i] && s1[i]
-                && s2[i] && i < n - 1)
-                i++;
-        return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i]
+		&& s2[i] && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
